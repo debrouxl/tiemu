bea80d5382531cb9fa5b2fbc9b7b388b289d44d5
Romain Liévin <roms@lievin.net>
2004-05-05 14:07:23 +0000

    UAE auto-generated files.
    git-svn-id: http://svn.tilp.info/repos/tiemu@98 4f589446-7ac1-0310-82c7-a055b08196ef

---
+++ tiemu/trunk/src/core/uae/cpu7.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "sysdeps.h"
+#include "uconfig.h"
+#include "options.h"
+#include "memory.h"
+#include "newcpu.h"
+#include "cputbl.h"
+void op_7000(ULONG opcode)
+{
+	ULONG srcreg = (LONG)(BYTE)((opcode & 255) >> 0);
+	ULONG dstreg = (opcode & 3584) >> 9;
+{{	ULONG src = srcreg;
+{	VFLG = CFLG = 0;
+	ZFLG = ((LONG)(src)) == 0;
+	NFLG = ((LONG)(src)) < 0;
+	regs.d[dstreg] = (src);
+}}}}
