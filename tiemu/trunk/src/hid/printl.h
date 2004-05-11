3883a24df19878e87fb722c4b62927579e690f77
Romain Liévin <roms@lievin.net>
2004-05-11 06:55:52 +0000

    Forgotten to add printl.
    git-svn-id: http://svn.tilp.info/repos/tiemu@138 4f589446-7ac1-0310-82c7-a055b08196ef

---
+++ tiemu/trunk/src/hid/printl.h
@@ -0,0 +1,39 @@
+/* Hey EMACS -*- linux-c -*- */
+/* $Id: cabl_int.h 651 2004-04-25 15:22:07Z roms $ */
+
+/*  TiEmu - an TI emulator
+ *
+ *  Originally written by Jonas Minsberg
+ *  Copyright (C) 2000, Thomas Corvazier, Romain Lievin
+ *  Copyright (C) 2001-2004, Romain Lievin
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __PRINTL_H__
+#define __PRINTL_H__
+
+#define	PRINT_STD	0
+#define PRINT_WRN	1
+#define PRINT_ERR	2
+
+/* Definition */
+typedef int (*TIEMU_PRINTL) (int level, const char *format, ...);
+
+/* Callback */
+extern TIEMU_PRINTL printl;
+
+
+#endif
