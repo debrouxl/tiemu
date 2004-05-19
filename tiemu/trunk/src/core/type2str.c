280faa285649476ed43c327d4651cbb54cbad32d
Romain Liévin <roms@lievin.net>
2004-05-19 21:09:56 +0000

    Updated header.
    git-svn-id: http://svn.tilp.info/repos/tiemu@232 4f589446-7ac1-0310-82c7-a055b08196ef

--- tiemu/trunk/src/core/type2str.c
+++ tiemu/trunk/src/core/type2str.c
@@ -1,19 +1,20 @@
 /* Hey EMACS -*- linux-c -*- */
-/* $Id: type2str.c 689 2004-05-09 20:29:44Z roms $ */
+/* $Id: main.c 231 2004-05-19 21:08:35Z roms $ */
 
-/*  libticables - Ti Link Cable library, a part of the TiLP project
- *  Copyright (C) 1999-2004  Romain Lievin
+/*  TiEmu - an TI emulator
+ *
+ *  Copyright (c) 2000, Thomas Corvazier, Romain Lievin
+ *  Copyright (c) 2001-2002, Romain Lievin, Julien Blache
+ *  Copyright (c) 2003-2004, Romain Liévin
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
- *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
+ *  GNU General Public License for more details. *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
