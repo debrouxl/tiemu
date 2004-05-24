18ecae8341c1047b851e59bf1b85a69059149b17
Romain Liévin <roms@lievin.net>
2004-05-24 19:22:26 +0000

    License headers updated.
    git-svn-id: http://svn.tilp.info/repos/tiemu@264 4f589446-7ac1-0310-82c7-a055b08196ef

--- tiemu/trunk/src/core/ti_hw/memory.c
+++ tiemu/trunk/src/core/ti_hw/memory.c
@@ -1,6 +1,11 @@
-/*  gtktiemu - a TI89/92/92+ emulator
- *  (c) Copyright 2000-2001, Romain Lievin and Thomas Corvazier
- *  (c) Copyright 2001-2002, Romain Lievin
+/* Hey EMACS -*- linux-c -*- */
+/* $Id: main.c 245 2004-05-23 20:45:43Z roms $ */
+
+/*  TiEmu - an TI emulator
+ *
+ *  Copyright (c) 2000, Thomas Corvazier, Romain Lievin
+ *  Copyright (c) 2001-2002, Romain Lievin, Julien Blache
+ *  Copyright (c) 2003-2004, Romain Liévin
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
