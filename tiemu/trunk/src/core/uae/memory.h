c858915d176825cdd8ab360eb4c4cf1130145918
Romain Liévin <roms@lievin.net>
2004-06-16 09:22:51 +0000

    Switched to std types.
    git-svn-id: http://svn.tilp.info/repos/tiemu@469 4f589446-7ac1-0310-82c7-a055b08196ef

--- tiemu/trunk/src/core/uae/memory.h
+++ tiemu/trunk/src/core/uae/memory.h
@@ -11,13 +11,13 @@ extern ULONG get_long(CPTR adr);
 extern void put_long(CPTR adr, ULONG arg);
 extern void put_word(CPTR adr, UWORD arg);
 extern void put_byte(CPTR adr, UBYTE arg);
-
+/*
 extern UBYTE io_get_byte(CPTR adr);
 extern UWORD io_get_word(CPTR adr);
 extern ULONG io_get_long(CPTR adr);
 extern void io_put_long(CPTR adr, ULONG arg);
 extern void io_put_word(CPTR adr, UWORD arg);
 extern void io_put_byte(CPTR adr, UBYTE arg);
-
+*/
 extern UBYTE *get_real_address(CPTR adr);
 extern int valid_address(CPTR adr, ULONG size);
