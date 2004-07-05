65ebe0322731a1fb343e771a739cf0f5d9fb94fc
Romain Liévin <roms@lievin.net>
2004-07-05 16:26:34 +0000

    ON key fully working on TI89 & TI92+.
    git-svn-id: http://svn.tilp.info/repos/tiemu@507 4f589446-7ac1-0310-82c7-a055b08196ef

--- tiemu/trunk/src/core/ti_hw/ports.c
+++ tiemu/trunk/src/core/ti_hw/ports.c
@@ -129,7 +129,7 @@ void io_put_byte(uint32_t addr, uint8_t arg)
             }
             
 			// turn off OSC1 (CPU), wake on int level 6 (ON key) and int level [5..1]
-			if (!(arg & 0x1f) && (tihw.calc_type != TI92)) 
+			if(!(arg & 0x10) && (tihw.calc_type != TI92))	// should be 0x1f, does not work otherwise
 				specialflags |= SPCFLAG_STOP; 
 			if(!(arg & 0x10) && tihw.calc_type == TI92)
 				specialflags |= SPCFLAG_STOP;
