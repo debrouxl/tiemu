23118d335ee5a7b4a172a1ba329a36d56e09acda
Romain Liévin <roms@lievin.net>
2005-02-16 14:45:58 +0000

    Fixed UAE Makefile and sysdeps.h for compilation under MinGW.
    git-svn-id: http://svn.tilp.info/repos/tiemu@833 4f589446-7ac1-0310-82c7-a055b08196ef

--- tiemu/trunk/src/core/uae/sysdeps.h
+++ tiemu/trunk/src/core/uae/sysdeps.h
@@ -5,10 +5,6 @@
 #ifndef SYSDEPS_H
 #define SYSDEPS_H
 
-#ifdef __MINGW32__
-#include <windows.h>
-#endif
-
 /* Set ULONG, LONG, UWORD, WORD, UBYTE & BYTE
  * 
  */
@@ -19,15 +15,11 @@
 
 /* If char has more then 8 bits, good night. */
 typedef unsigned char UBYTE;
-#ifndef __MINGW32__
 typedef signed char BYTE;
-#endif
 
 #if SIZEOF_SHORT == 2
 typedef unsigned short UWORD;
-#ifndef __MINGW32__
 typedef short WORD;
-#endif
 #elif SIZEOF_INT == 2
 typedef unsigned int UWORD;
 typedef int WORD;
@@ -35,7 +27,6 @@ typedef int WORD;
 #error No 2 byte type, you lose.
 #endif
 
-#ifndef __MINGW32__
 #if SIZEOF_INT == 4
 typedef unsigned int ULONG;
 typedef int LONG;
@@ -44,7 +35,6 @@ typedef unsigned long ULONG;
 typedef long LONG;
 #else
 #error No 4 byte type, you lose.
-#endif
 #endif
 
 typedef ULONG CPTR;
