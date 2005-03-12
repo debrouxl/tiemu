22b0280b36c3af7e546e6aa8f77344b737856aa5
Kevin Kofler <kevin.kofler@chello.at>
2005-03-12 06:34:30 +0000

    Allow building with -mwindows -D_WINDOWS on MinGW.
    git-svn-id: http://svn.tilp.info/repos/tiemu@937 4f589446-7ac1-0310-82c7-a055b08196ef

--- tiemu/trunk/src/gui/main.c
+++ tiemu/trunk/src/gui/main.c
@@ -280,6 +280,9 @@ int main(int argc, char **argv)
    then we use the 'WinMain' entry point.
 */
 #if defined(__WIN32__) && defined(_WINDOWS)// && !defined(_CONSOLE)
+#ifdef __MINGW32__
+#include <windows.h>
+#endif
 int APIENTRY WinMain(HINSTANCE hInstance,
                      HINSTANCE hPrevInstance,
                      LPSTR     lpCmdLine,
