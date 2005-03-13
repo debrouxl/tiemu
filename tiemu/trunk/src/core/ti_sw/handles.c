87ac98bb24a736f06dfb8d007cc6fb2ca3632f61
Romain Liévin <roms@lievin.net>
2005-03-13 16:57:41 +0000

    Fixed size of archived handles on PedRom.
    git-svn-id: http://svn.tilp.info/repos/tiemu@954 4f589446-7ac1-0310-82c7-a055b08196ef

--- tiemu/trunk/src/core/ti_sw/handles.c
+++ tiemu/trunk/src/core/ti_sw/handles.c
@@ -150,7 +150,7 @@ uint16_t heap_size(int handle)
 	heap_get_addr(&base);
 
 	addr = mem_rd_long(base + 4*handle);
-	if(!pedrom)
+	if(!pedrom || addr > rom_base)
 	{
 		size = mem_rd_word(addr - 2);
 		size &= ~(1 << 16);	// remove lock
