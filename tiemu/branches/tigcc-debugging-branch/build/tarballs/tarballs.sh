65f49b880bf92d860263015c5d36197fc9ad8ce6
Kevin Kofler <kevin.kofler@chello.at>
2005-04-30 19:38:04 +0000

    Create unique tarball names.
    git-svn-id: http://svn.tilp.info/repos/tiemu@1220 4f589446-7ac1-0310-82c7-a055b08196ef

--- tiemu/branches/tigcc-debugging-branch/build/tarballs/tarballs.sh
+++ tiemu/branches/tigcc-debugging-branch/build/tarballs/tarballs.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
-tar cfj libticables-svn-head.tar.bz2 libticables --exclude='\.svn' --exclude='.*/\.svn'
-tar cfj libtifiles-svn-head.tar.bz2 libtifiles --exclude='\.svn' --exclude='.*/\.svn'
-tar cfj libticalcs-svn-head.tar.bz2 libticalcs --exclude='\.svn' --exclude='.*/\.svn'
-tar cfj tiemu-tigcc-debugging-svn-head.tar.bz2 tiemu-tigcc-debugging --exclude='\.svn' --exclude='.*/\.svn'
-tar cfj tilp-svn-head.tar.bz2 tilp --exclude='\.svn' --exclude='.*/\.svn'
+tar cfj libticables-`date +%Y%m%d`.tar.bz2 libticables --exclude='\.svn' --exclude='.*/\.svn'
+tar cfj libtifiles-`date +%Y%m%d`.tar.bz2 libtifiles --exclude='\.svn' --exclude='.*/\.svn'
+tar cfj libticalcs-`date +%Y%m%d`.tar.bz2 libticalcs --exclude='\.svn' --exclude='.*/\.svn'
+tar cfj tiemu-tigcc-debugging-`date +%Y%m%d`.tar.bz2 tiemu-tigcc-debugging --exclude='\.svn' --exclude='.*/\.svn'
+tar cfj tilp-`date +%Y%m%d`.tar.bz2 tilp --exclude='\.svn' --exclude='.*/\.svn'
 
