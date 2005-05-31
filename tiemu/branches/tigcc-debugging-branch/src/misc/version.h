0966bc6df742fb4cc2fb869a52703c86e4a3279b
Kevin Kofler <kevin.kofler@chello.at>
2005-05-31 23:53:12 +0000

    Merge from trunk up to rev 1457.
    git-svn-id: http://svn.tilp.info/repos/tiemu@1458 4f589446-7ac1-0310-82c7-a055b08196ef

--- tiemu/branches/tigcc-debugging-branch/src/misc/version.h
+++ tiemu/branches/tigcc-debugging-branch/src/misc/version.h
@@ -20,7 +20,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *  Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
 #ifndef VERSION_H
@@ -36,7 +36,7 @@
 */
 
 #ifdef __WIN32__
-# define TIEMU_VERSION "2.00-rc1"		// For Win32
+# define TIEMU_VERSION "2.00-rc2"		// For Win32
 #else
 # define TIEMU_VERSION VERSION
 #endif
