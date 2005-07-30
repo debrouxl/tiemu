b14f422ead6ea44d92f7098558669dc6432f4b88
Kevin Kofler <kevin.kofler@chello.at>
2005-07-30 16:52:50 +0000

    Fix missing windows.h inclusion on MinGW. Minor changelog fixes while I was at it.
    git-svn-id: http://svn.tilp.info/repos/tiemu@1599 4f589446-7ac1-0310-82c7-a055b08196ef

--- tiemu/trunk/src/core/interface.c
+++ tiemu/trunk/src/core/interface.c
@@ -7,7 +7,7 @@
  *  Copyright (c) 2001-2003, Romain Lievin
  *  Copyright (c) 2003, Julien Blache
  *  Copyright (c) 2004, Romain Liévin
- *  Copyright (c) 2005, Romain Liévin
+ *  Copyright (c) 2005, Romain Liévin, Kevin Kofler
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -36,6 +36,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+
+#ifdef __MINGW32__
+#include <windows.h>
+#endif
 
 #include "libuae.h"
 #include "hw.h"
