074b4e384478b31d469848df5b448c66cf64cf1a
Romain Liévin <roms@lievin.net>
2006-05-24 09:51:16 +0000

    Added RELEASE file targeted for no gdb version.
    git-svn-id: http://svn.tilp.info/repos/tiemu@2108 4f589446-7ac1-0310-82c7-a055b08196ef

--- tiemu/trunk/build/InnoSetup/Output/update.bat
+++ tiemu/trunk/build/InnoSetup/Output/update.bat
@@ -1,5 +1,5 @@
 rm Release.txt
-copy C:\sources\roms\TiEmu3\RELEASE Release.txt
+copy C:\sources\roms\TiEmu3\RELEASE.nogdb Release.txt
 rm ChangeLog.txt
 copy C:\sources\roms\TiEmu3\ChangeLog ChangeLog.txt
 cls
