ce3a9dd73623fe901c2cd26ba94526880b93c758
Romain Liévin <roms@lievin.net>
2006-08-26 17:17:30 +0000

    Reworked user boxes: use GTK standard ones instead (look better).
    git-svn-id: http://svn.tilp.info/repos/tiemu@2216 4f589446-7ac1-0310-82c7-a055b08196ef

--- tiemu/trunk/src/gui/dboxes.c
+++ tiemu/trunk/src/gui/dboxes.c
@@ -34,11 +34,9 @@
 
 static gint ret_val = 0;
 
-int msg_box1(const gchar * title, gchar * message)
+static gint str2msg(const char *title)
 {
-	GtkWidget *dialog, *label;
-	gint result;
-	gint msg_type = -1;
+	gint msg_type = GTK_MESSAGE_INFO;
 
 	if (!strcmp(title, _("Information")))
 		msg_type = GTK_MESSAGE_INFO;
@@ -48,11 +46,22 @@ int msg_box1(const gchar * title, gchar * message)
 		msg_type = GTK_MESSAGE_QUESTION;
 	else if (!strcmp(title, _("Error")))
 		msg_type = GTK_MESSAGE_ERROR;
+	else
+		msg_type = GTK_MESSAGE_INFO;
 
+	return msg_type;
+}
+
+int msg_box1(const char *title, const char *message)
+{
+	GtkWidget *dialog, *label;
+	gint result;
+	gint msg_type;
+
+	msg_type = str2msg(title);
 	if (msg_type != -1) 
 	{
-		dialog =
-		    gtk_message_dialog_new(NULL, GTK_DIALOG_MODAL,
+		dialog = gtk_message_dialog_new(NULL, GTK_DIALOG_MODAL,
 					   msg_type, GTK_BUTTONS_CLOSE,
 					   message);
 		gtk_dialog_run(GTK_DIALOG(dialog));
@@ -70,9 +79,6 @@ int msg_box1(const gchar * title, gchar * message)
 		gtk_container_add(GTK_CONTAINER(GTK_DIALOG(dialog)->vbox), label);
 		gtk_widget_show(label);
 
-		while (gtk_events_pending())
-			gtk_main_iteration();
-
 		result = gtk_dialog_run(GTK_DIALOG(dialog));
 		switch (result) 
 		{
@@ -87,23 +93,13 @@ int msg_box1(const gchar * title, gchar * message)
 	return 0;
 }
 
-gint msg_box2(const char *title, char *message)
+gint msg_box2(const char *title, const char *message)
 {
 	GtkWidget *dialog;
 	gint result;
-	gint msg_type = -1;
+	gint msg_type;
 
-	if (!strcmp(title, _("Information")))
-		msg_type = GTK_MESSAGE_INFO;
-	else if (!strcmp(title, _("Warning")))
-		msg_type = GTK_MESSAGE_WARNING;
-	else if (!strcmp(title, _("Question")))
-		msg_type = GTK_MESSAGE_QUESTION;
-	else if (!strcmp(title, _("Error")))
-		msg_type = GTK_MESSAGE_ERROR;
-	else
-		msg_type = GTK_MESSAGE_INFO;
-
+	msg_type = str2msg(title);
 	dialog = gtk_message_dialog_new(NULL, GTK_DIALOG_MODAL, msg_type,
 				   GTK_BUTTONS_OK_CANCEL, message);
 	gtk_dialog_set_default_response(GTK_DIALOG(dialog),
@@ -125,23 +121,22 @@ gint msg_box2(const char *title, char *message)
 	return 0;
 }
 
-gint msg_box3(const char *title, char *message, const char *button1,
+gint msg_box3(const char *title, const char *message, const char *button1,
 	      const char *button2, const char *button3)
 {
-	GtkWidget *dialog, *label;
+	GtkWidget *dialog;
 	gint result;
+	gint msg_type;
 
-	dialog = gtk_dialog_new_with_buttons(title, GTK_WINDOW(NULL),
-					     (GtkDialogFlags)
-					     (GTK_DIALOG_MODAL),
-					     button1, GTK_RESPONSE_YES,
-					     button2, GTK_RESPONSE_NO,
-					     button3, GTK_RESPONSE_CANCEL, 
-						 NULL);
-	gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_CANCEL);
-	label = gtk_label_new(message);
-	gtk_container_add(GTK_CONTAINER(GTK_DIALOG(dialog)->vbox), label);
-	gtk_widget_show(label);
+	msg_type = str2msg(title);
+	dialog = gtk_message_dialog_new(NULL, GTK_DIALOG_MODAL, msg_type,
+				   GTK_BUTTONS_NONE, message);
+	gtk_dialog_add_button(GTK_DIALOG(dialog), button1, GTK_RESPONSE_YES);
+	gtk_dialog_add_button(GTK_DIALOG(dialog), button2, GTK_RESPONSE_NO);
+	gtk_dialog_add_button(GTK_DIALOG(dialog), button3, GTK_RESPONSE_CANCEL);
+
+	gtk_dialog_set_default_response(GTK_DIALOG(dialog),
+					GTK_RESPONSE_CANCEL);
 
 	result = gtk_dialog_run(GTK_DIALOG(dialog));
 	switch (result) 
@@ -159,27 +154,24 @@ gint msg_box3(const char *title, char *message, const char *button1,
 		return BUTTON3;
 		break;
 	}
-
+	
 	return 0;
 }
 
-gint msg_box4(const char *title, char *message)
+gint msg_box4(const char *title, const char *message)
 {
-	GtkWidget *dialog, *label;
+	GtkWidget *dialog;
 	gint result;
+	gint msg_type;
 
-	dialog = gtk_dialog_new_with_buttons(title, GTK_WINDOW(NULL),
-					     (GtkDialogFlags)
-					     (GTK_DIALOG_MODAL),
-					     GTK_STOCK_GO_FORWARD,
-					     GTK_RESPONSE_OK,
-					     GTK_STOCK_CANCEL,
-					     GTK_RESPONSE_CANCEL, NULL);
+	msg_type = str2msg(title);
+	dialog = gtk_message_dialog_new(NULL, GTK_DIALOG_MODAL, msg_type,
+				   GTK_BUTTONS_NONE, message);
+	gtk_dialog_add_button(GTK_DIALOG(dialog), GTK_STOCK_GO_FORWARD, GTK_RESPONSE_OK);
+	gtk_dialog_add_button(GTK_DIALOG(dialog), GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL);
+
 	gtk_dialog_set_default_response(GTK_DIALOG(dialog),
 					GTK_RESPONSE_CANCEL);
-	label = gtk_label_new(message);
-	gtk_container_add(GTK_CONTAINER(GTK_DIALOG(dialog)->vbox), label);
-	gtk_widget_show(label);
 
 	result = gtk_dialog_run(GTK_DIALOG(dialog));
 	switch (result) 
@@ -199,8 +191,7 @@ gint msg_box4(const char *title, char *message)
 
 
 /* Create the dialog box entry and wait */
-char *msg_entry(const char *title, const char *message,
-		const char *content)
+char *msg_entry(const char *title, const char *message, const char *content)
 {
 	GladeXML *xml;
 	GtkWidget *data;
@@ -209,11 +200,12 @@ char *msg_entry(const char *title, const char *message,
 	gchar *ret = NULL;
 	gint result;
 	ret_val = 0;
+
 	xml = glade_xml_new
 	    (tilp_paths_build_glade("user_boxes-2.glade"), "entry_dbox",
 	     PACKAGE);
 	if (!xml)
-		g_error(_("%s: GUI loading failed !\n"), __FILE__);
+		g_error("dboxes.c: GUI loading failed !\n");
 	glade_xml_signal_autoconnect(xml);
 
 	dbox = data = glade_xml_get_widget(xml, "entry_dbox");
--- tiemu/trunk/src/gui/dboxes.h
+++ tiemu/trunk/src/gui/dboxes.h
@@ -33,14 +33,12 @@
 #define BUTTON2  2
 #define BUTTON3  3
 
-int msg_box1(const gchar * title, gchar * message);
-int msg_box2(const char *title, char *message);
-int msg_box3(const char *title, char *message,
-	      const char *button1, const char *button2,
-	      const char *button3);
-int msg_box4(const char *title, char *message);
-char *msg_entry(const char *title,
-		const char *message, const char *content);
+int msg_box1(const gchar * title, const gchar * message);
+int msg_box2(const char *title, const char *message);
+int msg_box3(const char *title, const char *message,
+			 const char *button1, const char *button2, const char *button3);
+int msg_box4(const char *title, const char *message);
+char *msg_entry(const char *title, const char *message, const char *content);
 
 #define msg_box	msg_box1
 
