34eb2b700e59e731e35ac9311a36f888af446cec
Romain Liévin <roms@lievin.net>
2006-12-05 17:11:36 +0000

    Fixed typo and formating.
    git-svn-id: http://svn.tilp.info/repos/tiemu@2291 4f589446-7ac1-0310-82c7-a055b08196ef

--- tiemu/trunk/src/core/ti_hw/dbus.c
+++ tiemu/trunk/src/core/ti_hw/dbus.c
@@ -418,7 +418,7 @@ int send_ti_file(const char *filename)
 	ret = ticalcs_calc_isready(calc_handle);
 	printf("ret = %i\n", ret);
 
-	if(ret == 257 /*ERR_NOT_READY*/)
+	if(ret == 257)	/* ERR_NOT_READY = 257 */
 	{
 		switch(tihw.calc_type)
 		{
@@ -434,7 +434,6 @@ int send_ti_file(const char *filename)
 
 		default: break;
 		}
-		printf("ret = %i\n", ret);
 
 		do_cpu(5);
 
@@ -509,11 +508,14 @@ int ti68k_linkport_ready(void)
 {
 	int ret;
 	int pedrom;
+
 	// Some OS versions lock up if they get a readiness probe too early in the boot cycle.
 	hw_m68k_run(1250000,5000000);
+
 	// Hack: PedroM doesn't like resetting the link flag here, so handle it specially.
 	//       Test this now because this might not be set yet if the OS hasn't booted up yet.
 	pedrom = (mem_rd_word(0x32) == (('R'<<8)+'O'));
+
 	// Block sending and receiving.
 	if (sip || rip)
 		return 0;
