193c57c00d3f417ea32a13553eda261631d570a8
Kevin Kofler <kevin.kofler@chello.at>
2007-02-17 21:39:17 +0000

    Zap md5.sum in cleangdbsources.sh.
    git-svn-id: http://svn.tilp.info/repos/tiemu@2354 4f589446-7ac1-0310-82c7-a055b08196ef

--- tiemu/trunk/src/cleangdbsources.sh
+++ tiemu/trunk/src/cleangdbsources.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # This is used on an exploded GDB tarball when doing merges (removes the directories we don't ship).
-rm -rf contrib cpu gdb/gdbtk/plugins sim/arm sim/cris sim/d10v sim/erc32 sim/frv sim/h8300 sim/igen sim/iq2000 sim/m32c sim/m32r sim/m68hc11 sim/mcore sim/mips sim/mn10300 sim/ppc sim/sh sim/v850 sim/testsuite/common sim/testsuite/config sim/testsuite/d10v-elf sim/testsuite/frv-elf sim/testsuite/lib sim/testsuite/m32r-elf sim/testsuite/mips64el-elf sim/testsuite/sim
+rm -rf contrib cpu gdb/gdbtk/plugins sim/arm sim/cris sim/d10v sim/erc32 sim/frv sim/h8300 sim/igen sim/iq2000 sim/m32c sim/m32r sim/m68hc11 sim/mcore sim/mips sim/mn10300 sim/ppc sim/sh sim/v850 sim/testsuite/common sim/testsuite/config sim/testsuite/d10v-elf sim/testsuite/frv-elf sim/testsuite/lib sim/testsuite/m32r-elf sim/testsuite/mips64el-elf sim/testsuite/sim md5.sum
