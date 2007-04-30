6f85ccf6a00256c1f03f4dcc5767f5e9030f7eca
Kevin Kofler <kevin.kofler@chello.at>
2007-04-30 07:36:37 +0000

    Remove snapshot tarball script now that actual releases are used.
    git-svn-id: http://svn.tilp.info/repos/tiemu@2469 4f589446-7ac1-0310-82c7-a055b08196ef

--- tiemu/trunk/build/tarballs/tarballs.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-tar -cj --exclude='\.svn' --exclude='.*/\.svn' -f libticables2-`date +%Y%m%d`.tar.bz2 libticables
-tar -cj --exclude='\.svn' --exclude='.*/\.svn' -f libticonv-`date +%Y%m%d`.tar.bz2 libticonv
-tar -cj --exclude='\.svn' --exclude='.*/\.svn' -f libtifiles2-`date +%Y%m%d`.tar.bz2 libtifiles
-tar -cj --exclude='\.svn' --exclude='.*/\.svn' -f libticalcs2-`date +%Y%m%d`.tar.bz2 libticalcs
-tar -cj --exclude='\.svn' --exclude='.*/\.svn' -f tiemu3-`date +%Y%m%d`.tar.bz2 tiemu
-tar -cj --exclude='\.svn' --exclude='.*/\.svn' -f tilp2-`date +%Y%m%d`.tar.bz2 tilp
-
