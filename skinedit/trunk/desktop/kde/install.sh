c5b37a43fcaa5bbe3058bcf6c6668c492eab4326
Romain Liévin <roms@lievin.net>
2007-05-07 20:37:15 +0000

    Fixing export problem.
    git-svn-id: http://svn.tilp.info/repos/tiemu@2474 4f589446-7ac1-0310-82c7-a055b08196ef

---
+++ skinedit/trunk/desktop/kde/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "Beware: script intented to developers: unsupported!"
+cp skinedit.desktop /usr/share/applications
+cp x-skinedit.desktop /usr/share/mimelnk/application
+echo "Files copied."
