12e7cb58740e0c6fc3c76fe0807196c504c09580
Romain Liévin <roms@lievin.net>
2007-05-08 12:33:50 +0000

    Pending commit ... commit'ed.
    git-svn-id: http://svn.tilp.info/repos/tiemu@2476 4f589446-7ac1-0310-82c7-a055b08196ef

---
+++ tiemu/trunk/desktop/kde/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "Beware: script intented to developers: unsupported!"
+cp tiemup.desktop /usr/share/applications
+cp x-tiemu-*.desktop /usr/share/mimelnk/application
+echo "Files copied."
