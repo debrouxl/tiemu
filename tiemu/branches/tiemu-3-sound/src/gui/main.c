1546d420e5170f9ea111c7ae5f7a8840e4ce07b1
Peter Fernandes <supersonicandtails@gmail.com>
2007-06-22 02:32:28 +0000

    updated terminal copyright output
    git-svn-id: http://svn.tilp.info/repos/tiemu@2549 4f589446-7ac1-0310-82c7-a055b08196ef

--- tiemu/branches/tiemu-3-sound/src/gui/main.c
+++ tiemu/branches/tiemu-3-sound/src/gui/main.c
@@ -187,7 +187,7 @@ int main(int argc, char **argv)
 
 #if WITH_KDE
     splash_screen_set_label(_("Initializing KDE..."));
-    sp_kde_init(argc, argv, "tiemu", _("TiEmu"), VERSION, _("TI calculator emulator"), "Copyright (c) 2000, Thomas Corvazier, Romain Lievin\nCopyright (c) 2001-2002, Romain Lievin, Julien Blache\nCopyright (c) 2003-2004, Romain Lievin\nCopyright (c) 2005-2007, Romain Lievin, Kevin Kofler", "http://lpg.ticalc.org/prj_tiemu/", "gtktiemu-users@lists.sf.net");
+    sp_kde_init(argc, argv, "tiemu", _("TiEmu"), VERSION, _("TI calculator emulator"), "Copyright (c) 2000, Thomas Corvazier, Romain Lievin\nCopyright (c) 2001-2002, Romain Lievin, Julien Blache\nCopyright (c) 2003-2004, Romain Lievin\nCopyright (c) 2005-2007, Romain Lievin, Kevin Kofler\nCopyright (c) 2007, Peter Fernandes", "http://lpg.ticalc.org/prj_tiemu/", "gtktiemu-users@lists.sf.net");
     atexit(sp_kde_finish);
     g_timeout_add(26, sp_kde_process_qt_events, NULL);
 #endif
