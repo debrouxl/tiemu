13e65fa20c76ddfeda52930fc96809ca976e4c42
Romain Liévin <roms@lievin.net>
2009-05-29 14:04:35 +0000

    Preparing release...
    git-svn-id: http://svn.tilp.info/repos/tiemu@2842 4f589446-7ac1-0310-82c7-a055b08196ef

--- tiemu/trunk/build/InnoSetup/Output/update.bat
+++ tiemu/trunk/build/InnoSetup/Output/update.bat
@@ -1,5 +1,5 @@
 rm Release.txt
-copy C:\sources\roms\TiEmu3\RELEASE Release.txt
+copy C:\lpg\TiEmu3\RELEASE Release.txt
 rm ChangeLog.txt
-copy C:\sources\roms\TiEmu3\ChangeLog ChangeLog.txt
+copy C:\lpg\TiEmu3\ChangeLog ChangeLog.txt
 cls
