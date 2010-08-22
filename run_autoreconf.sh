94a07f5ea8c6f74e135c9361aed3c6845545722f
Lionel Debroux <lionel_debroux@yahoo.fr>
2010-08-22 18:57:53 +0000

    skinedit, tiemu: use AM_GNU_GETTEXT_VERSION to use a consistent version of gettext (namely 0.16, released in late 2006).
    git-svn-id: http://svn.tilp.info/repos/tiemu@2859 4f589446-7ac1-0310-82c7-a055b08196ef

--- run_autoreconf.sh
+++ run_autoreconf.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 for module in skinedit tiemu; do
 echo Updating "$module"
-cd "$module/trunk"; autoreconf -f -i || exit 1; cd ../..
+cd "$module/trunk"; rm m4/*.m4; autoreconf -f -i || exit 1; cd ../..
 done
